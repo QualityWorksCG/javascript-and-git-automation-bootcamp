dc4b28e26c65c06a682cbe4833d8f638d396f5ee
kxngHunter <65135986+kxngHunter@users.noreply.github.com>
2022-10-22 19:36:54 -0500

    Update arrayIncludes5Or2 function

--- Romain-Hunter/04/exercise.test.js
+++ Romain-Hunter/04/exercise.test.js
@@ -1,7 +1,14 @@
 describe("04", () => {
   test("Check if an array of 3 items, contains a 5 or 2", () => {
     const arrayIncludes5Or2 = (array) => {
-      return array.includes(5) || array.includes(2);
+      let found = false;
+      for (const num of array) {
+        if (num === 2 || num === 5) {
+          found = true;
+          break;
+        }
+      }
+      return found;
     };
 
     // Test cases
