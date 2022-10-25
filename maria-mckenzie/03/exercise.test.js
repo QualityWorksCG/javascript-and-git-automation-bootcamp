d62a565881aa21fd0754091095598ab79dcecf9d
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-10-25 06:34:04 -0500

    Completed exercise 03

--- maria-mckenzie/03/exercise.test.js
+++ maria-mckenzie/03/exercise.test.js
@@ -2,8 +2,10 @@ describe('03', () => {
     it('03. Check if any of two number is 6, if the sum of them makes 6 or if the difference is 6', () => {
 
         function isSix(x, y) {
-            // Your code here
-
+            if (x+y == 6 || x-y == 6 || y-x == 6) {
+                return true;
+            } 
+            return false;
         }
 
         // Test cases
