14f102789df4fd1edbd13c68b58d9fb4cb922130
Adaine Campbell <acampbell@etechja.com>
2022-10-28 16:30:57 -0500

    Assignment Complete

---
+++ adaine-campbell/06/exercise.test.js
@@ -0,0 +1,66 @@
+describe('06', () => {
+
+    // Tip: Use setTimeout to delay the execution of marking
+
+    const listOfPapers = [
+        {
+            subject: "Maths",
+            wasSubmitted: true,
+            markPaper: () => {
+                // add a promise here that resolves after 2 seconds
+                // and print "Maths paper marked"
+                return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(console.log ('Maths paper marked'));
+                }, 2000); });
+                
+            }
+        },
+        {
+            subject: "Geology",
+            wasSubmitted: true,
+            markPaper: () => {
+                // add a promise here that resolves after 2 seconds
+                // and print "Geology paper marked"
+                return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(console.log ('Geology paper marked'));
+                }, 2000); });
+            }
+        },
+        {
+            subject: "Social Studies",
+            wasSubmitted: false,
+            markPaper: () => {
+                // add a promise here that resolves after 2 seconds
+                // and print "Social Studies paper marked"
+                return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(console.log ('Social Studies paper marked'));
+                    
+                }, 2000); });
+            }
+        },
+    ]
+
+    it('Check if a paper was submitted, and if yes, wait for it to be marked', async () => {
+        const spyOnLog = vi.spyOn(console, 'log');
+
+        // Your code here
+        for (let index = 0; index < listOfPapers.length; index++){
+            const paper=listOfPapers[index]
+
+            if(paper.wasSubmitted){
+            await paper.markPaper()
+            }
+            
+        }
+
+        expect(spyOnLog).toHaveBeenCalledWith("Maths paper marked");
+        expect(spyOnLog).toHaveBeenCalledWith("Geology paper marked");
+        expect(spyOnLog).not.toHaveBeenCalledWith("Social Studies paper marked");
+        expect(listOfPapers[0].markPaper()).toBeInstanceOf(Promise);
+        expect(listOfPapers[1].markPaper()).toBeInstanceOf(Promise);
+        expect(listOfPapers[2].markPaper()).toBeInstanceOf(Promise);
+    });
+});
