681ec2d6ecab0ee33af641584ebb9b7598186fd5
Abi Matthews-Gordon <60277772+AbiMatthews-Gordon@users.noreply.github.com>
2022-10-28 19:34:08 -0500

    Exercises 6 passed all tests

--- abi-matthews-gordon/06/exercise.test.js
+++ abi-matthews-gordon/06/exercise.test.js
@@ -7,8 +7,20 @@ describe('06', () => {
             subject: "Maths",
             wasSubmitted: true,
             markPaper: () => {
+
                 // add a promise here that resolves after 2 seconds
                 // and print "Maths paper marked"
+                return new Promise((resolve, reject) => {
+
+                    setTimeout(() => resolve("Math paper marked"), 2000);
+
+                }).then(
+
+                    function(result){
+
+                        console.log(result);
+                    }
+                );
             }
         },
         {
@@ -17,6 +29,17 @@ describe('06', () => {
             markPaper: () => {
                 // add a promise here that resolves after 2 seconds
                 // and print "Geology paper marked"
+                return new Promise((resolve, reject) => {
+
+                    setTimeout(() => resolve("Geology paper marked"), 2000);
+
+                }).then(
+
+                    function(result){
+
+                        console.log(result);
+                    }
+                );
             }
         },
         {
@@ -25,6 +48,17 @@ describe('06', () => {
             markPaper: () => {
                 // add a promise here that resolves after 2 seconds
                 // and print "Social Studies paper marked"
+                return new Promise((resolve, reject) => {
+
+                    setTimeout(() => resolve("Social Studies paper marked"), 2000);
+
+                }).then(
+
+                    function(result){
+
+                        console.log(result);
+                    }
+                );
             }
         },
     ]
@@ -32,14 +66,24 @@ describe('06', () => {
     it('Check if a paper was submitted, and if yes, wait for it to be marked', async () => {
         const spyOnLog = vi.spyOn(console, 'log');
 
-        // Your code here
+        for(let paper of listOfPapers){
 
+            if(paper.wasSubmitted){
 
-        expect(spyOnLog).toHaveBeenCalledWith("Maths paper marked");
-        expect(spyOnLog).toHaveBeenCalledWith("Geology paper marked");
-        expect(spyOnLog).not.toHaveBeenCalledWith("Social Studies paper marked");
-        expect(listOfPapers[0].markPaper()).toBeInstanceOf(Promise);
-        expect(listOfPapers[1].markPaper()).toBeInstanceOf(Promise);
-        expect(listOfPapers[2].markPaper()).toBeInstanceOf(Promise);
+                paper.markPaper();
+            }
+        }
+
+        setTimeout(() => {
+            
+            expect(spyOnLog).toHaveBeenCalledWith("Maths paper marked");
+            expect(spyOnLog).toHaveBeenCalledWith("Geology paper marked");
+            expect(spyOnLog).not.toHaveBeenCalledWith("Social Studies paper marked");
+            expect(listOfPapers[0].markPaper()).toBeInstanceOf(Promise);
+            expect(listOfPapers[1].markPaper()).toBeInstanceOf(Promise);
+            expect(listOfPapers[2].markPaper()).toBeInstanceOf(Promise);
+
+        }, 2050); //given more time to ensure it doesn't run before the code it's waiting on
+
     });
 });
