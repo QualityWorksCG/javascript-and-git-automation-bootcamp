74898f57fc6c74392a17d114c87b1e2f7a845bf8
Abi Matthews-Gordon <60277772+AbiMatthews-Gordon@users.noreply.github.com>
2022-10-28 20:34:46 -0500

    Exercises 8 passed all tests

--- abi-matthews-gordon/08/exercise.test.js
+++ abi-matthews-gordon/08/exercise.test.js
@@ -15,29 +15,73 @@ describe('08', () => {
      */
 
     test('Create a Vehicle class, and create different type of vehicles that extends', () => {
-        // Your code here
+
+        class Vehicle {
+
+            constructor(wheels, bodyType) {
+                this.wheels = wheels;
+                this.bodyType = bodyType;
+            }
+        }
+
+        class Car extends Vehicle {
+
+            constructor(color, wheels, bodyType){
+                super(wheels, bodyType);
+                this.color = color;
+            }
+
+            whatIsMyName(){
+                return "I am a " + this.bodyType;
+            }
+        }
+
+        class Truck extends Vehicle {
+
+            constructor(color, wheels, bodyType){
+                super(wheels, bodyType);
+                this.color = color;
+            }
+
+            whatIsMyName(){
+                return "I am a " + this.bodyType;
+            }
+        }
+
+        class Motorcycle extends Vehicle {
+
+            constructor(color, wheels, bodyType){
+                super(wheels, bodyType);
+                this.color = color;
+            }
+
+            whatIsMyName(){
+                return "I am a " + this.bodyType;
+            }
+        }
 
 
-        // Expected outcome for the child classes
-        const car = new Car('red', 4, 'sedan');
-        const truck = new Truck('blue', 6, 'pickup');
-        const motorcycle = new Motorcycle('green', 2, 'motorcycle');
+
+            // Expected outcome for the child classes
+            const car = new Car('red', 4, 'sedan');
+            const truck = new Truck('blue', 6, 'pickup');
+            const motorcycle = new Motorcycle('green', 2, 'motorcycle');
 
 
-        // Test cases
-        expect(car.wheels).toBe(4);
-        expect(car.bodyType).toBe('sedan');
-        expect(car.color).toBe('red');
-        expect(car.whatIsMyName()).toBe('I am a sedan');
+            // Test cases
+            expect(car.wheels).toBe(4);
+            expect(car.bodyType).toBe('sedan');
+            expect(car.color).toBe('red');
+            expect(car.whatIsMyName()).toBe('I am a sedan');
 
-        expect(truck.wheels).toBe(6);
-        expect(truck.bodyType).toBe('pickup');
-        expect(truck.color).toBe('blue');
-        expect(truck.whatIsMyName()).toBe('I am a pickup');
+            expect(truck.wheels).toBe(6);
+            expect(truck.bodyType).toBe('pickup');
+            expect(truck.color).toBe('blue');
+            expect(truck.whatIsMyName()).toBe('I am a pickup');
 
-        expect(motorcycle.wheels).toBe(2);
-        expect(motorcycle.bodyType).toBe('motorcycle');
-        expect(motorcycle.color).toBe('green');
-        expect(motorcycle.whatIsMyName()).toBe('I am a motorcycle');
-    });
+            expect(motorcycle.wheels).toBe(2);
+            expect(motorcycle.bodyType).toBe('motorcycle');
+            expect(motorcycle.color).toBe('green');
+            expect(motorcycle.whatIsMyName()).toBe('I am a motorcycle');
+        });
 });
