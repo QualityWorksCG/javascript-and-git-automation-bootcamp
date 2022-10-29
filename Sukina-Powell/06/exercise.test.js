cfa715cdbbdb26f0af6f37001b0e600acbb001e9
SPowell1 <powellsukina@gmail.com>
2022-10-28 19:49:55 -0500

    assignment

---
+++ Sukina-Powell/06/exercise.test.js
@@ -0,0 +1,75 @@
+describe('06', () => {
+
+    // Tip: Use setTimeout to delay the execution of marking
+
+    const listOfPapers = [
+        {
+            subject: "Maths",
+            wasSubmitted: true,
+            markPaper: () => 
+            {
+                const promise = new Promise((resolve, reject) => 
+                {
+                    setTimeout(() => 
+                    {
+                       resolve("Maths paper marked");
+                    }, 2000);
+                })
+                return promise;
+                
+            }
+        },
+        {
+            subject: "Geology",
+            wasSubmitted: true,
+            markPaper: () => {
+                const promise = new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        resolve("Geology paper marked");
+                    }, 2000);
+
+                })
+                return promise;
+            }
+        },
+        {
+            subject: "Social Studies",
+            wasSubmitted: false,
+            markPaper: () => 
+            {
+                const promise= new Promise((resolve, reject) => 
+                {
+                    setTimeout(() => 
+                    {
+                       resolve("Social Studies paper marked");
+                    }, 2000);
+
+                 })
+                 return promise;
+
+            }
+        },
+    ]
+
+   it('Check if a paper was submitted, and if yes, wait for it to be marked', async () => 
+    {
+        const spyOnLog = vi.spyOn(console, 'log');
+        for (const paper of listOfPapers) 
+        {
+            if(paper.wasSubmitted==true)
+            {
+                const result= await paper.markPaper();
+                console.log(result);
+            }
+        }
+        
+
+
+        expect(spyOnLog).toHaveBeenCalledWith("Maths paper marked");
+        expect(spyOnLog).toHaveBeenCalledWith("Geology paper marked");
+        expect(spyOnLog).not.toHaveBeenCalledWith("Social Studies paper marked");
+        expect(listOfPapers[0].markPaper()).toBeInstanceOf(Promise);
+        expect(listOfPapers[1].markPaper()).toBeInstanceOf(Promise);
+        expect(listOfPapers[2].markPaper()).toBeInstanceOf(Promise);
+    });
+});
---
+++ Sukina-Powell/08/exercise.test.js
@@ -0,0 +1,92 @@
+describe('08', () => {
+    /**
+     * You are the manager of a new Vehicle company
+     *
+     *  And you need to create different kind of cars to list in your inventory. 
+     *
+     *  1. Create a Vehicle parent
+     *      - wheels as a property
+     *      - bodyType as a property
+     *
+     *  2. Create different kind of vehicles from the parent
+     *      - each vehicle should have a color
+     *      - each vehicle should have a method called "whatIsMyName" that return the bodyType of the vehicle
+     *      - Example: "I am a sedan"
+     */
+
+    test('Create a Vehicle class, and create different type of vehicles that extends', () => {
+        class Vehicle
+        {
+            constructor(wheels,bodyType)
+            {
+                this.wheels= wheels;
+                this.bodyType= bodyType;
+            }
+        }
+        
+        class Car extends Vehicle
+        {
+            constructor(color,cWheels,cBodyType)
+            {
+                super(cWheels,cBodyType);
+                this.color=color;
+            }
+            whatIsMyName(bodyType)
+            {
+                return `I am a ${this.bodyType}`;
+            }
+
+        }
+
+        class Truck extends Vehicle
+        {
+            constructor(color,tWheels,tBodyType)
+            {
+                super(tWheels,tBodyType);
+                this.color=color;
+            }
+            whatIsMyName(bodyType)
+            {
+                return `I am a ${this.bodyType}`;
+            }
+
+        }
+
+        class Motorcycle extends Vehicle
+        {
+            constructor(color,mWheels,mBodyType)
+            {
+                super(mWheels,mBodyType);
+                this.color=color;
+            }
+            whatIsMyName(bodyType)
+            {
+                return `I am a ${this.bodyType}`;
+            }
+
+        }
+
+       
+        // Expected outcome for the child classes
+        const car = new Car('red', 4, 'sedan');
+        const truck = new Truck('blue', 6, 'pickup');
+        const motorcycle = new Motorcycle('green', 2, 'motorcycle');
+
+
+        // Test cases
+        expect(car.wheels).toBe(4);
+        expect(car.bodyType).toBe('sedan');
+        expect(car.color).toBe('red');
+        expect(car.whatIsMyName()).toBe('I am a sedan');
+
+        expect(truck.wheels).toBe(6);
+        expect(truck.bodyType).toBe('pickup');
+        expect(truck.color).toBe('blue');
+        expect(truck.whatIsMyName()).toBe('I am a pickup');
+
+        expect(motorcycle.wheels).toBe(2);
+        expect(motorcycle.bodyType).toBe('motorcycle');
+        expect(motorcycle.color).toBe('green');
+        expect(motorcycle.whatIsMyName()).toBe('I am a motorcycle');
+    });
+});
